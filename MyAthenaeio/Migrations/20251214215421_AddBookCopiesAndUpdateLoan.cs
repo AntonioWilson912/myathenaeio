a5367ee44b53ca4aaed1bf63fb110709aae24036
Antonio Wilson <109255146+AntonioWilson912@users.noreply.github.com>
2025-12-14 16:57:04 -0500

    Implement CRUD methods in `LibraryService` for all EF Core models Apply migrations to `Book` and `Loan` models

---
+++ MyAthenaeio/Migrations/20251214215421_AddBookCopiesAndUpdateLoan.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyAthenaeio.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddBookCopiesAndUpdateLoan : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ReturnDate",
+                table: "Loans",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "TEXT");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DateAdded",
+                table: "Books",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "TEXT");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Copies",
+                table: "Books",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Copies",
+                table: "Books");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ReturnDate",
+                table: "Loans",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                oldClrType: typeof(DateTime),
+                oldType: "TEXT",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DateAdded",
+                table: "Books",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                oldClrType: typeof(DateTime),
+                oldType: "TEXT",
+                oldNullable: true);
+        }
+    }
+}
